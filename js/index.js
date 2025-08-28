c666766ac0bf93aa8fc3a8914888f7038c980159
giash459 <mdgiash999ctg@gmail.com>
2025-08-28 21:44:35 +0600

    added increase heart button

--- js/index.js
+++ js/index.js
@@ -1,9 +1,32 @@
 // heart function
-
 const hearts = document.getElementsByClassName("heart-icon");
 for(const i of hearts){
     i.addEventListener("click", function(e){
         e.preventDefault();
-        console.log("heart-icon clicked")
+        const heartIncrease = parseInt(document.getElementById("heart-increase").innerText)
+        const heartIncreaseField = heartIncrease + 1;
+        document.getElementById("heart-increase").innerText = heartIncreaseField 
     })
 }
+
+// reuse function
+function setInnerTextNumber(id){
+    const textNumber = parseInt(document.getElementById(id).innerText)
+    return textNumber;
+} 
+
+// call function
+document.getElementById("call-1").addEventListener("click", function(e){
+    e.preventDefault()
+    const title = document.getElementById("sub-title").innerText
+    const description = document.getElementById("sub-description").innerText
+    alert(title + " " + description)
+    const point = parseInt(document.getElementById("point-btn").innerText)
+    const pointValue = point - 20;
+     if(pointValue < 0){
+        alert("No available balance")
+        return
+    }
+    document.getElementById("point-btn").innerText = pointValue
+   
+})
